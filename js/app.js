fda2bd748c0a4a83c53d19ea0a8451523c7fb659
Shahil19 <basharshahil@gmail.com>
2022-02-17 05:55:34 +0600

    savings and remaing balance completed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
-document.getElementById("calculate-btn").addEventListener("click", function () {
-  // get income amount
+// get income by function
+function getIncome() {
   const incomeAmountText = document.getElementById("income-amount");
   const incomeAmount = incomeAmountText.value;
+  return parseFloat(incomeAmount);
+}
+
+// get current balance by function
+function getBalance(incomeAmount, totalSpending) {
+  const currentBalanceText = document.getElementById("balance-amount");
+  const currentBalance = currentBalanceText.innerText;
+  currentBalanceText.innerText = parseFloat(incomeAmount) - totalSpending;
+  return currentBalance;
+}
+
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const incomeAmount = getIncome();
 
   // get expenses
   const foodExpense = parseFloat(document.getElementById("food-expense").value);
@@ -12,17 +25,38 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
 
   // get total spending
   const totalSpending = foodExpense + rentExpense + clothExpense;
-  console.log(totalSpending);
 
   // get current expense
   const currentExpense = document.getElementById("total-expense");
   currentExpense.innerText = totalSpending;
 
-  // update balance
-  const currentBalance = document.getElementById("balance-amount");
-  currentBalance.innerText = parseFloat(incomeAmount) - totalSpending;
+  // update remaining balance
+  const currentBalance = getBalance(incomeAmount, totalSpending);
 });
 
 document.getElementById("savings-btn").addEventListener("click", function () {
-  console.log("clicked");
+  // get income
+  const incomeAmount = getIncome();
+  console.log(incomeAmount);
+
+  // get savings percentage
+  const savingsInputText = document.getElementById("savings-input").value;
+  const savingsInput = parseFloat(savingsInputText);
+  const savingsPercentage = savingsInput / 100;
+
+  // total savings amount
+  const savingsAmount = incomeAmount * savingsPercentage;
+
+  // get current savings amount and update total savings
+  const currentSavingsAmountText = document.getElementById("savings-amount");
+  const currentSavingsAmount = parseFloat(currentSavingsAmountText.innerText);
+  currentSavingsAmountText.innerText = savingsAmount;
+
+  // get remaining balance after expenses
+  const currentBalance = parseFloat(getBalance());
+
+  // get total remaining
+  const totalRemainingText = document.getElementById("remaining-total");
+  const totalRemaining = parseFloat(totalRemainingText.innerText);
+  totalRemainingText.innerText = currentBalance - savingsAmount;
 });
